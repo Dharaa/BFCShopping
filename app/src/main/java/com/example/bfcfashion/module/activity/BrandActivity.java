9ec6d4e30f27b5b0cbc2238af933aca6591d8037
Vipin Kumar Thakur <vipin12thakur@gmail.com>
2020-11-25 10:57:20 +0530

    Made brand screen

--- app/src/main/java/com/example/bfcfashion/module/activity/BrandActivity.java
+++ app/src/main/java/com/example/bfcfashion/module/activity/BrandActivity.java
@@ -12,5 +12,8 @@ public class BrandActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_brand);
+
+
+        //
     }
 }
